8735a4df6bcc0263f7b6e7ca0ecd0b9f184e7c32
Mohamed Brahimi <momohb2007@gmail.com>
2026-05-10 12:27:52 -0400

    Added Vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import Hero from "@/sections/Hero";
 import Projects from "@/sections/Projects"
 import Footer from "@/components/Footer";
 
+import { Analytics } from '@vercel/analytics/next';
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white">
+      <Analytics />
+      
       <Navbar />
 
       <Hero />
