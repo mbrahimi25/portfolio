bb1043d7abf4882a364fd40627c08471dec05a11
Mohamed Brahimi <momohb2007@gmail.com>
2026-05-10 18:08:14 -0400

    Added Vercel speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Projects from "@/sections/Projects"
 import Footer from "@/components/Footer";
 
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white">
       <Analytics />
-      
+      <SpeedInsights />
+
       <Navbar />
 
       <Hero />
