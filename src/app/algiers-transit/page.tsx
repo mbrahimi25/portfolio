149be2195521abdaedfb3abd1fbde9fdcbb99cfa
Mohamed Brahimi <momohb2007@gmail.com>
2026-05-20 19:04:41 -0400

    Updated Algiers transit page with new transit diagram

--- src/app/algiers-transit/page.tsx
+++ src/app/algiers-transit/page.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import ArticleHero from "@/sections/ArticleHero";
 
 import { SiQgis, SiOpenstreetmap } from "react-icons/si";
+import { BiLogoAdobe } from "react-icons/bi";
 
 
 import Image from "next/image";
@@ -19,6 +20,7 @@ export default function TtcGtfsArticle() {
       >
         <SiQgis size={25} className="transition duration-200 hover:scale-120"/>
         <SiOpenstreetmap size={25} className="transition duration-200 hover:scale-120"/>
+        <BiLogoAdobe size={25} className="transition duration-200 hover:scale-120"/>
       </ArticleHero>
 
       {/* Main Article Content */}
@@ -41,6 +43,7 @@ export default function TtcGtfsArticle() {
             <hr className="border-white/30 my-8" />
 
             <p className="mt-6 leading-8 text-white/70">
+              I wanted to begin by mapping Algiers' transit system in QGIS. <br/><br/>
               I started off by finding a shapefile of Algeria's <i>wilayat</i>, or provinces.
               Doing so was relatively straightforward. <br/>
               In case I needed it, I also found a shapefile containing <i>daïrat</i>, or districts, which are subdivisions of provinces in Algeria. <br/>
@@ -53,16 +56,46 @@ export default function TtcGtfsArticle() {
 
             <Image
             src="/algiers-transit-1.png"
-            alt="Algiers transit image 1"
+            alt="Algiers public transit mapped in QGIS"
             width={500}
             height={400}
             className="rounded-2xl mt-6"/>
 
+            <hr className="border-white/30 my-8" />
+
+            <p className="mt-6 leading-8 text-white/70">
+              After making a basic map of the Algiers region and its trams, trains, metro, and aerial tramways, 
+              I realised that there was no official transit diagram combining these modes of transport into one graphic.
+              Each mode of transport is operated by a different operator, so interchanges are an issue as well. <br/>
+              Because of this, I decided to try my hand at designing a transit diagram for the <i>wilaya</i> of Algiers using Adobe Illustrator. <br/><br/>
+              Finding a way to organize and place the lines and stations on my graphic was a challenge due to the shape of Algiers and the non-gridlike nature of its roads. <br/>
+              I decided to draw the metro and tramway systems as mostly going right-left instead of drawing them as continuously curving. 
+              In this sense, my diagram is slightly misleading, but the point of a transit diagram is not to be geographically accurate, 
+              but rather to show how each station is accessible relative to other stations and routes. <br/><br/>
+              With this in mind, I began working on my diagram using Adobe Illustrator. After some design work, this is what I ended up with:
+            </p>
+
+            <Image
+            src="/algiers-transit-2.png"
+            alt="Algiers public transit map, designed in Adobe Illustrator"
+            width={1550}
+            height={850}
+            className="rounded-2xl mt-6"/>
+            
+            <p className="mt-6 leading-8 text-white/70">
+              As you can see, the diagram is geographically inaccurate. The final stretch of the commuter rail and subway (Gare d'Alger, Place des Martyrs)
+              is technically north-south, but is displayed in my map as left-right.
+              I decided to design it this way as it was easier to read on a graphic and didn't waste too much space. <br/><br/>
+
+              Overall, I was proud of the diagram I designed, especially considering the confusing maps which exist online and the lack
+              of official diagrams and maps from the authorities operating the Algiers transit systems.
+              My next step was to make this map interactive (like I did with my TTC diagram)
+              while learning from the mistakes and limitations of my previous interactive transit map.
+            </p>
+
             <p className="mt-6 leading-8 text-white/70">
               I am still working on this project. Check back over the next few days to see any new updates!
             </p>
-            
-
 
           </div>
         </div>
